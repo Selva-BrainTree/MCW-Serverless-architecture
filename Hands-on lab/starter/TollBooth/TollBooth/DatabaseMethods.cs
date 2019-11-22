cd78743612d5fdcfcc2d7abda761d82e1fc6de74
Selva Kumar Muthu Krishnan <Selva.Krishnan@aa.com>
2019-11-22 16:08:23 -0600

    Test Commit

--- Hands-on lab/starter/TollBooth/TollBooth/DatabaseMethods.cs
+++ Hands-on lab/starter/TollBooth/TollBooth/DatabaseMethods.cs
@@ -42,8 +42,8 @@ namespace TollBooth
                 // TODO 5: Retrieve a List of LicensePlateDataDocument objects from the collectionLink where the exported value is false.
                 // COMPLETE: licensePlates = _client.CreateDocumentQuery ...
                 licensePlates = _client.CreateDocumentQuery<LicensePlateDataDocument>(collectionLink, new FeedOptions() { EnableCrossPartitionQuery = true, MaxItemCount = 100 })
-        .Where(l => l.exported == false)
-        .ToList();
+                                       .Where(l => l.exported == false)
+                                       .ToList();
                 // TODO 6: Remove the line below.
                // licensePlates = new List<LicensePlateDataDocument>();
             }
